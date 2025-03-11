5a114220eb9eef70e9db94d15997cea7dcb2ee9e
MYIGT\leew <warren.lee@igt.com>
2025-03-11 23:41:34 +0000

    Initial commit

---
+++ src/main/js/game/template/textStyles.js
@@ -0,0 +1,106 @@
+define({
+    plaqueMessageSmall: {},
+    buttonText: {},
+
+    winningNumbersTitle: {
+        fontFamily: 'oswald',
+        fontWeight: 'bold',
+        fontSize: 36,
+        fill: 'fontColourLuckyNumberTitle',
+        stroke: "#231f20",
+        strokeThickness: 5
+    },
+    playerNumbersTitle: {
+        fontFamily: 'oswald',
+        fontWeight: 'bold',
+        fontSize: 36,
+        fill: 'fontColourYourNumberTitle',
+        stroke: "#231f20",
+        strokeThickness: 5
+
+    },
+
+    prizeValueNoWin: {
+        fontFamily: 'oswald',
+        fontSize: 30,
+        fontWeight: 'bold',
+        fill: 'fontColourCashValueNoWin',
+    },
+    prizeValueWin: {
+        fontFamily: 'oswald',
+        fontSize: 30,
+        fontWeight: 'bold',
+        fill: 'fontColourCashValueWin',
+    },
+
+    winUpTo: {
+        fontFamily: 'oswald',
+        fontSize: 40,
+        fontWeight: 'bold',
+        fill: 'fontColourWinUpToTitle',
+        stroke: "#231f20",
+        strokeThickness: 5
+    },
+
+    bonusLabel: {
+        fontFamily: 'oswald',
+        fontSize: 44,
+        fontWeight: 'bold',
+        fill: 'fontColourBonusCover',
+        stroke: "#231f20",
+        strokeThickness: 5
+    },
+    bonusWin: {
+        fontFamily: 'oswald',
+        fontSize: 44,
+        fontWeight: 'bold',
+        dropShadow: true,
+        dropShadowAlpha: 0.8,
+        dropShadowAngle: 1.5,
+        dropShadowColor: "#3a1e01",
+        dropShadowDistance: 2,
+        fill: [
+            "#fff27d",
+            "#fbcc40"
+        ],
+        fillGradientStops: [
+            0.2,
+            0.5
+        ],
+        lineJoin: "bevel",
+        stroke: "#542801",
+        strokeThickness: 3
+    },
+    bonusNoWin: {
+        fontFamily: 'oswald',
+        fontSize: 44,
+        fontWeight: 'bold',
+        fill: 'fontColourBonusNoWin',
+    },
+
+    winPlaqueValue: {
+        fontFamily: 'oswald',
+        fontSize: 120,
+        fontWeight: 'bold',
+        dropShadow: true,
+        dropShadowAlpha: 0.8,
+        dropShadowAngle: 1.5,
+        dropShadowColor: "#3a1e01",
+        dropShadowDistance: 2,
+        fill: [
+            "#fff27d",
+            "#fbcc40"
+        ],
+        fillGradientStops: [
+            0.2,
+            0.5
+        ],
+        lineJoin: "bevel",
+        stroke: "#542801",
+        strokeThickness: 3
+    },
+
+
+});
+
+
